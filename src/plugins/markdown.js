3b28e039129736ca72fba9fd658c3f7b2f939e0a
LeungJZ <375082677@qq.com>
2018-08-22 16:50:51 +0800

    add markdown-it 对 tasklist 的支持。

--- src/plugins/markdown.js
+++ src/plugins/markdown.js
@@ -1,10 +1,12 @@
 import markdown from 'markdown-it'
 import markdownEmoji from 'markdown-it-emoji'
+import markdownCheckbox from 'markdown-it-task-lists'
 
 const md = markdown({
     linkify: true
 })
     .use(markdownEmoji)
+    .use(markdownCheckbox)
 
 // window.$markdown = md
 export default md
